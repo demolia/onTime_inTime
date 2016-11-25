54bf861b6ef035628be7cafe6d76646664eec590
Jimmy <jimmyvoskuil@msn.com>
2016-11-25 10:45:43 +0100

    added log-in

---
+++ static/main.js
@@ -0,0 +1,22 @@
+$( document ).ready(function() {
+   $(".divregister").hide()
+
+  $('.registerform').click(function(){ showregister(); return false; })
+
+function showregister() {
+	// To Hide Div Block
+ $(".divlogin").hide()
+	// To show Div Block
+ $(".divregister").show()
+}
+
+  $('.loginform').click(function(){ showlogin(); return false; })
+
+function showlogin() {
+	// To Hide Div Block
+ $(".divregister").hide()
+	// To show Div Block
+ $(".divlogin").show()
+}
+
+});
